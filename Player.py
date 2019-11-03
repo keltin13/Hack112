0b60ba882ea3d532d70e00158c123e8a51ff3838
suddenlykevin-temp <kevinx@andrew.cmu.edu>
2019-11-03 01:39:10 -0400

    Player Movement
    WASD and QE controls
    need to add boundary detection

--- Player.py
+++ Player.py
@@ -2,15 +2,95 @@
 ##      Suicide Squad - Hack112         ##
 ## Keltin Grimes, Alex White, Kevin Xie ##
 ##########################################
+import copy
 
 class Player(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, app, x, y):
+        self.app = app
+        self.pos = [x, y]
         self.width = 10
         self.height = 20
+        self.swimStamina = 100
+        self.gravity = -1
+        self.underwater = False
+        self.waterCount = self.swimStamina
+        self.jumpForce = 5
+        self.movementSpeed = 5
+        self.velocity = [0,0]
+    
+    def reset(self):
+        self.pos = copy.copy(self.app.init)
+        self.velocity = [0,0]
 
     def draw(self, canvas):
-        canvas.create_rectangle(self.x-self.width//2, self.y-self.height//2,
-                                self.x+self.width//2, self.y+self.height//2,
+        x, y = self.pos[0], self.pos[1]
+        canvas.create_rectangle(x-self.width//2, y-self.height//2,
+                                x+self.width//2, y+self.height//2,
                                 fill = 'black')
+    
+    def up(self):
+        if self.standingOnPlatform():
+            self.velocity[1] = -self.jumpForce
+        elif self.underwater:
+            self.velocity[1] = self.movementSpeed*0.8
+    
+    def down(self):
+        if self.underwater:
+            self.velocity[1] = self.movementSpeed*0.8
+    
+    def left(self):
+        if self.underwater:
+            self.velocity[0] = -self.movementSpeed*0.8
+        else:
+            self.velocity[0] = -self.movementSpeed
+    
+    def right(self):
+        if self.underwater:
+            self.velocity[0] = self.movementSpeed*0.8
+        else:
+            self.velocity[0] = self.movementSpeed
+    
+    def returnToPlatform(self):
+        while self.standingOnPlatform():
+            self.pos[1] -= 1
+        self.pos[1] += 1
+
+    def standingOnPlatform(self):
+        return (self.app.checkInBounds(self.pos[0], self.pos[1] + 1 + self.height//2)
+                or self.pos[1] + self.height//2 + 1 > self.app.height)
+    
+    def returnToBounds(self):
+        if self.pos[1] + self.height//2 >= self.app.height:
+            self.pos[1] = self.app.height - self.height//2
+        elif self.pos[1] - self.height//2 <= 0:
+            self.pos[1] = self.height//2
+        if self.pos[0] + self.width//2 >= self.app.width:
+            self.pos[0] = self.app.width - self.width//2
+        elif self.pos[0] - self.width//2 <= 0:
+            self.pos[0] = self.width//2
+    
+class WaterBoy(Player):
+    def __init__(self, app, x, y):
+        super().__init__(app, x, y)
+        self.swimStamina = 300
+        self.waterCount = self.swimStamina
+
+class GravityBoy(Player):
+    def __init__(self, app, x, y):
+        super().__init__(app, x, y) 
+        self.gravity = 1
+        self.jumpForce = -5
+    
+    def standingOnPlatform(self):
+        return (self.app.checkInBounds(self.pos[0], self.pos[1] - 1 - self.height//2)
+                or self.pos[1] - self.height//2 - 1 <= 0)
+
+    def returnToPlatform(self):
+        while self.standingOnPlatform():
+            self.pos[1] += 1
+        self.pos[1] -= 1
+
+class JumpMan(Player):
+    def __init__(self, app, x, y):
+        super().__init__(app, x, y)
+        self.jumpForce = 10
--- game.py
+++ game.py
@@ -6,7 +6,7 @@
 from tkinter import *
 from cmu_112_graphics import *
 
-from Player import Player
+from Player import *
 from Boundary import Boundary
 from Water import Water
 
@@ -30,27 +30,91 @@ class GameMode(Mode):
     def appStarted(mode):
         pass
 
+    def keyPressed(mode, event):
+        if event.key == 'q':
+            mode.activePlayer = (mode.activePlayer - 1) % mode.playerTypes
+            mode.players[mode.activePlayer].reset()
+        elif event.key == 'e':
+            mode.activePlayer = (mode.activePlayer + 1) % mode.playerTypes
+            mode.players[mode.activePlayer].reset()
+        elif event.key == 'Space':
+            mode.players[mode.activePlayer].up()
+        elif event.key in mode.activeKeys:
+            mode.activeKeys[event.key] = True
+    
+    def keyReleased(mode, event):
+        if event.key in mode.activeKeys:
+            mode.activeKeys[event.key] = False
+    
+    def checkInBounds(mode, x, y):
+        for b in mode.boundaries:
+            if b.left <= x <= b.right and b.top <= y <= b.bottom:
+                return True
+        return False
+    
+    def checkIntersect(mode, player, boundsList):
+        for b in boundsList:
+            if b.left <= player.x <= b.right and b.top <= player.y <= b.bottom:
+                return True
+        return False
+    
+    def updatePhysics(mode):
+        player = mode.players[mode.activePlayer]
+        # if mode.checkIntersect(player, mode.waters):
+        #     player.underwater = True
+        # else:
+        #     player.underwater = False
+        for i in range(len(player.pos)):
+            player.pos[i] += player.velocity[i]
+        if player.underwater:
+            player.velocity = [0,0]
+            player.waterCount -= 1
+        else:
+            player.waterCount = player.swimStamina
+            player.velocity[0] = 0
+            if player.standingOnPlatform():
+                player.returnToPlatform()
+                player.velocity[1] = 0
+            else:    
+                player.velocity[1] -= player.gravity
+            player.returnToBounds()
+
     def timerFired(mode):
-        pass
+        if mode.activeKeys['a'] == True:
+            mode.players[mode.activePlayer].left()
+        elif mode.activeKeys['d'] == True:
+            mode.players[mode.activePlayer].right()
+        elif mode.activeKeys['s'] == True:
+            mode.players[mode.activePlayer].down()
+        elif mode.activeKeys['w'] == True:
+            mode.players[mode.activePlayer].up()
+        mode.updatePhysics()
 
     def redrawAll(mode, canvas):
+        player = mode.players[mode.activePlayer]
         canvas.create_rectangle(0, 0, mode.width, mode.height, fill = 'white')
         canvas.create_text(mode.width//2, mode.height//2, fill = 'black',
                         text = "The game is here")
         for boundary in mode.boundaries:
             boundary.draw(canvas)
-        mode.player.draw(canvas)
+        player.draw(canvas)
 
 class Introduction(GameMode):
     def __init__(self):
         super().__init__()
         self.createBoundaries()
-        self.player = Player(50, 350)
-
+        self.init = [50, 350]
+        self.players = [Player(self, self.init[0], self.init[1]), 
+                        GravityBoy(self, self.init[0], self.init[1]),
+                        JumpMan(self, self.init[0], self.init[1])]
+        self.playerTypes = len(self.players)
+        self.activePlayer = 0
+        self.activeKeys = {'a': False, 'd': False, 'w': False, 's': False}
+        
     def createBoundaries(self):
         self.boundaries = set()
         self.boundaries.add(Boundary('1', 0, 200, 100, 225))
-        self.boundaries.add(Boundary('2', 0, 400, 100, 425))
+        self.boundaries.add(Boundary('2', 0, 450, 100, 475))
 
 class Level1(GameMode):
     def __init__(self):
